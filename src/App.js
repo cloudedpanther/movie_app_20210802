881ca8a4f492b727e4bc18797dacdac2ea7aeb36
cloudedpanther <cloudedpanther@DESKTOP-NI3V40T.localdomain>
2021-08-03 22:07:08 +0900

    Practicing using JSX with props and proptypes.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+function Food({ name, stringLength }) {
+  return <div>
+    <h3>I love {name}.</h3>
+    <h4>The length of this string '{name}' is {stringLength}.</h4>
+  </div>
+}
 
-function Food({ fav }) {
-  return <h3>I love {fav}.</h3>
+const foodILike = [
+  {
+    id: 1,
+    name: 'kimbap',
+    stringLength: 6
+  },
+  {
+    id: 2,
+    name: 'namul',
+    stringLength: 5
+  },
+  {
+    id: 3,
+    name: 'hanugalbi',
+    stringLength: 9
+  },
+  {
+    id: 4,
+    name: 'jajangmyeon',
+    stringLength: 11
+  }
+];
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  stringLength: PropTypes.number.isRequired
 }
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!!!</h1>
-      <Food fav="kimchi" />
+      {foodILike.map(food => <Food key={food.id} name={food.name} stringLength={food.stringLength} />)}
     </div>
   );
 }
