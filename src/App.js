929a92def4fded5017eaf497eb7144798844ef37
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-08-04 19:43:29 +0900

    Practicing Classes: state and setState()

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food({ name, stringLength }) {
-  return <div>
-    <h3>I love {name}.</h3>
-    <h4>The length of the name '{name}' is {stringLength}.</h4>
-  </div>
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'kimbap',
-    stringLength: 6
-  },
-  {
-    id: 2,
-    name: 'namul',
-    stringLength: 5
-  },
-  {
-    id: 3,
-    name: 'hanugalbi',
-    stringLength: 9
-  },
-  {
-    id: 4,
-    name: 'jajangmyeon',
-    stringLength: 11
+class App extends React.Component {
+  state = {
+    count: 0
   }
-];
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  stringLength: PropTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(food => <Food key={food.id} name={food.name} stringLength={food.stringLength} />)}
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  }
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  }
+  render() {
+    return <div>
+      <h1>My favorite number is number {this.state.count}.</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+  }
 }
 
 export default App;
