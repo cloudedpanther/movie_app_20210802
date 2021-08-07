37a37bacd5705e59af024a4a2b129062aa2c58e4
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-08-07 13:39:38 +0900

    Fetching and Rendering Movies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
+import axios from "axios";
+import Movies from "./Movies";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  }
+  async getMovies() {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
   }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready!"}</div>
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movies => <Movies key={movies.id} id={movies.id} year={movies.year} title={movies.title} summary={movies.summary} poster={movies.medium_cover_image} />)}</div>
   }
 }
 
